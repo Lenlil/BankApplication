6e32412e9abb62e016242f9c36132a6a5798fb34
Lenlil <55378996+Lenlil@users.noreply.github.com>
2020-04-27 10:36:46 +0200

    Start + reggat Identity

---
+++ Bank/Bank/Data/DatabaseInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank.Data
+{
+    public class DatabaseInitializer
+    {
+        public void Initialize(ApplicationDbContext context)
+        {
+            //context.Database.EnsureCreated();
+            context.Database.Migrate();
+            SeedData(context);
+        }
+        private void SeedData(ApplicationDbContext context)
+        {
+            //Om vi vill lägga till testdata gör vi det här genom SeedData..
+          
+            //context.SaveChanges();
+        }
+    }
+}
