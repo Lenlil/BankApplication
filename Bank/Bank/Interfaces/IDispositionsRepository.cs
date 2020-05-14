cf32feaaf3bc4f97618160bfc4dc8019fadc51e3
Lenlil <55378996+Lenlil@users.noreply.github.com>
2020-05-14 10:37:15 +0200

    Dispositions Rep + Interface added

---
+++ Bank/Bank/Interfaces/IDispositionsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank.Interfaces
+{
+    public interface IDispositionsRepository
+    {
+        IQueryable<Dispositions> GetAll();
+        Dispositions GetOneByID(int accountId);
+        void Create(Dispositions account);
+        void Update(Dispositions account);
+    }
+}
---
+++ Bank/Bank/Repositories/DispositionsDbRepository.cs
@@ -0,0 +1,42 @@
+﻿using Bank.Data;
+using Bank.Interfaces;
+using Bank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank.Repositories
+{
+    public class DispositionsDbRepository : IDispositionsRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public DispositionsDbRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Dispositions disposition)
+        {
+            dbContext.Dispositions.Add(disposition);
+            dbContext.SaveChanges();
+        }
+
+        public IQueryable<Dispositions> GetAll()
+        {
+            return dbContext.Dispositions;
+        }
+
+        public Dispositions GetOneByID(int customerId)
+        {
+            return dbContext.Dispositions.Where(r => r.CustomerId == customerId).FirstOrDefault();
+        }
+
+        public void Update(Dispositions disposition)
+        {
+            dbContext.Dispositions.Update(disposition);
+            dbContext.SaveChanges();
+        }
+    }
+}
