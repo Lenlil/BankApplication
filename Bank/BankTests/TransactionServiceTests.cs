8681da1e4416b38ae202aea9d2e373aacb510731
Lenlil <55378996+Lenlil@users.noreply.github.com>
2020-05-26 14:54:36 +0200

    test för transfer within bank klar

--- Bank/BankTests/TransactionServiceTests.cs
+++ Bank/BankTests/TransactionServiceTests.cs
@@ -157,6 +157,88 @@ namespace BankTests
             Assert.AreEqual(expectedTransaction.Account, createdTransaction.Account);
         }
 
+        [TestMethod]
+        public void Transfer_within_bank_transactions_are_created_ok()
+        {
+            List<Accounts> accounts = ctx.Accounts.ToList();
+            var fromAccount = accounts[0];
+            fromAccount.Balance = 1000;
+            ctx.Accounts.Update(fromAccount);
+            ctx.SaveChanges();
+
+            var toAccount = accounts[1];
+            var oldToAccountBalance = toAccount.Balance;
+
+            var model = new TransferThisBankTransactionViewModel
+            {
+                Date = DateTime.Now.Date,
+                OldAccountBalance = fromAccount.Balance,
+                Type = "Debit",
+                Operation = "Remittance to Another Account",
+                Amount = 500,
+                FromAccountId = fromAccount.AccountId,
+                Symbol = null,
+                Bank = "Bitcoin Bank",
+                ToAccountId = toAccount.AccountId
+            };
+
+            var expectedFromTransaction = new Transactions
+            {
+                AccountId = model.FromAccountId,
+                Date = model.Date,
+                Balance = model.OldAccountBalance - model.Amount,
+                Type = model.Type,
+                Operation = model.Operation,
+                Amount = -model.Amount,
+                Symbol = model.Symbol,
+                Bank = model.Bank,
+                Account = toAccount.AccountId.ToString()
+            };
+
+            var expectedToTransaction = new Transactions
+            {
+                AccountId = model.ToAccountId,
+                Date = model.Date,
+                Balance = oldToAccountBalance + model.Amount,
+                Type = "Credit",
+                Operation = "Collection from Another Account",
+                Amount = model.Amount,
+                Symbol = model.Symbol,
+                Bank = model.Bank,
+                Account = model.FromAccountId.ToString()
+            };
+
+            sut.CreateTransferThisBankFromAccountTransaction(model);
+            sut.CreateTransferThisBankToAccountTransaction(model);
+
+            IQueryable<Transactions> transactions = ctx.Transactions;
+            var toTransactionID = transactions.Max(x => x.TransactionId);
+            var createdToTransaction = transactions.Where(x => x.TransactionId == toTransactionID).FirstOrDefault();
+
+            var fromTransactionID = toTransactionID - 1;
+            var createdFromTransaction = transactions.Where(x => x.TransactionId == fromTransactionID).FirstOrDefault();                    
+                       
+            Assert.AreEqual(expectedFromTransaction.AccountId, createdFromTransaction.AccountId);
+            Assert.AreEqual(expectedFromTransaction.Date, createdFromTransaction.Date);
+            Assert.AreEqual(expectedFromTransaction.Balance, createdFromTransaction.Balance);
+            Assert.AreEqual(expectedFromTransaction.Type, createdFromTransaction.Type);
+            Assert.AreEqual(expectedFromTransaction.Operation, createdFromTransaction.Operation);
+            Assert.AreEqual(expectedFromTransaction.Amount, createdFromTransaction.Amount);
+            Assert.AreEqual(expectedFromTransaction.Symbol, createdFromTransaction.Symbol);
+            Assert.AreEqual(expectedFromTransaction.Bank, createdFromTransaction.Bank);
+            Assert.AreEqual(expectedFromTransaction.Account, createdFromTransaction.Account);
+
+            Assert.AreEqual(expectedToTransaction.AccountId, createdToTransaction.AccountId);
+            Assert.AreEqual(expectedToTransaction.Date, createdToTransaction.Date);
+            Assert.AreEqual(expectedToTransaction.Balance, createdToTransaction.Balance);
+            Assert.AreEqual(expectedToTransaction.Type, createdToTransaction.Type);
+            Assert.AreEqual(expectedToTransaction.Operation, createdToTransaction.Operation);
+            Assert.AreEqual(expectedToTransaction.Amount, createdToTransaction.Amount);
+            Assert.AreEqual(expectedToTransaction.Symbol, createdToTransaction.Symbol);
+            Assert.AreEqual(expectedToTransaction.Bank, createdToTransaction.Bank);
+            Assert.AreEqual(expectedToTransaction.Account, createdToTransaction.Account);
+        }
+
 
         [TestMethod]
         public void When_withdrawing_can_not_take_more_money_than_balance_on_account()
